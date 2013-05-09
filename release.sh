82d24d705ead774179f9e118f55087fa93b639cd
Pierre-Alexandre Meyer <pierre@ning.com>
2013-05-08 17:23:41 -0700

    release.sh: update script
    See details at https://github.com/killbill/killbill-paypal-express-plugin/wiki/Killbill-Paypal-Express-plugin-Release
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- release.sh
+++ release.sh
@@ -1,4 +1,14 @@
+set -e
+
 VERSION=`grep -E '<version>([0-9]+\.[0-9]+\.[0-9]+)</version>' pom.xml | sed 's/[\t \n]*<version>\(.*\)<\/version>[\t \n]*/\1/'`
+if [ "$VERSION" != "$(cat $PWD/VERSION)" ]; then
+  echo "Unable to release: make sure the versions in pom.xml and VERSION match"
+  exit 1
+fi
+
+echo "Pushing the gem to Rubygems"
+rake release
+
 ARTIFACT="$PWD/pkg/killbill-paypal-express-$VERSION.tar.gz"
 echo "Pushing $ARTIFACT to Maven Central"
 mvn gpg:sign-and-deploy-file \
