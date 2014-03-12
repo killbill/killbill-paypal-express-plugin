8fcf11fc44edc53f6e0419af3d032a83dfbd4821
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-03-12 12:41:47 -0400

    pom.xml: change groupId to org.kill-bill.billing.plugin.ruby
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- release.sh
+++ release.sh
@@ -31,7 +31,7 @@ rake killbill:package
 ARTIFACT="$PWD/pkg/killbill-paypal-express-$VERSION.tar.gz"
 echo "Pushing $ARTIFACT to Maven Central"
 mvn gpg:sign-and-deploy-file \
-    -DgroupId=com.ning.killbill.ruby \
+    -DgroupId=org.kill-bill.billing.plugin.ruby \
     -DartifactId=paypal-express-plugin \
     -Dversion=$VERSION \
     -Dpackaging=tar.gz \
