8cce407702e73365bd83fa307fd5ea0b759d2806
Alena Dudzinskaya <alenadudz@gmail.com>
2017-02-24 10:42:41 -0800

    Make review setting configurable in buildFormDescriptor

--- lib/paypal_express/api.rb
+++ lib/paypal_express/api.rb
@@ -266,7 +266,8 @@ module Killbill #:nodoc:
       def to_express_checkout_url(response, kb_tenant_id, options = {})
         payment_processor_account_id = options[:payment_processor_account_id] || :default
         gateway                      = lookup_gateway(payment_processor_account_id, kb_tenant_id)
-        gateway.redirect_url_for(response.token, :review => false)
+        review                       = options.has_key?(:review) ? options[:review] : false
+        gateway.redirect_url_for(response.token, :review => review)
       end
 
       protected
